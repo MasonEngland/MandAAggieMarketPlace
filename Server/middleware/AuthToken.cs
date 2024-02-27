095126f41178e8ebeb55070f0046b03f53153a34
Mason England <masonengland01@gmail.com>
2024-02-27 12:56:08 -0700

    Added token auth

---
+++ Server/middleware/AuthToken.cs
@@ -0,0 +1,68 @@
+using JWT.Builder;
+using JWT.Algorithms;
+
+
+namespace Server.Middleware;
+
+public class AuthToken : IMiddleware
+{
+    public async Task InvokeAsync(HttpContext context, RequestDelegate next)
+    {
+
+        string path = context.Request.Path;
+
+        if (path.Contains("/Auth/Create") || path.Contains("/Auth/Login"))
+        {
+            await next(context);
+            return;
+        }
+
+        string secret = Environment.GetEnvironmentVariable("ACCESS_TOKEN_SECRET")!;
+        string? headers = context.Request.Headers.Authorization;
+
+        if (headers == null)
+        {
+            await context.Response.WriteAsync("Auth header was null");
+            return;
+        }
+
+        try
+        {
+            string authHeader = headers.Split(" ")[1];
+            var decoded = JwtBuilder
+                .Create()
+                .WithAlgorithm(new HMACSHA256Algorithm())
+                .WithSecret(secret)
+                .Decode(authHeader);
+            
+
+            if (decoded == null)
+            {
+                await context.Response.WriteAsync("token invalid");
+                context.Response.StatusCode = 401;
+                return;
+            }
+
+            IDictionary<object, object?> item = new Dictionary<object, object?>
+            {
+                {"tokenData", decoded}
+            };
+
+            context.Items = item;
+            await next(context);
+        } catch (Exception err)
+        {
+            System.Console.WriteLine(err.Message);
+            context.Response.StatusCode = 500;
+            return;
+        }
+    }
+}
+
+public static class AuthTokenExtensions
+{
+    public static IApplicationBuilder UseAuthToken(this IApplicationBuilder builder)
+    {
+        return builder.UseMiddleware<AuthToken>();
+    }
+}
